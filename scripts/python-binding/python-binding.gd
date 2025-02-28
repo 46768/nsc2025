bd1cb17c19ecb788e169697bb7a69e876d601b59
46768 <46768@bodin.ac.th>
2025-03-01 01:41:54 +0700

    Python server initialization

--- scripts/python-binding/python-binding.gd
+++ scripts/python-binding/python-binding.gd
@@ -1,23 +1,31 @@
 extends Node
 
+
 @export_range(49152, 65535) var server_port: int = 56440
 
-var thread: Thread
+
+# Python environment variables
+var python_env_packed: String = "res://python-binding/python-packed.pypck"
+var python_env_dirname: String = "user://python-env"
+
+# Server variables
+var server_thread: Thread
+const SERVER_PATH: String = "res://python-binding/server.py"
 var server_output = []
 
-func _start_python_ws_server(port: int):
-	print("Started python websocket server on port ", port)
-	var server_path: String = ProjectSettings.globalize_path(
-		"res://python-binding/server.py"
-	)
+func _start_python_ws_server(server_path: String, port: int):
+	var gpath = ProjectSettings.globalize_path(server_path)
 	OS.execute(
-		"python3", [server_path, "%d" % port],
+		"python3", [gpath, "%d" % port],
 		server_output, true
 	)
 	for i in server_output:
 		print(i)
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	thread = Thread.new()
-	thread.start(_start_python_ws_server.bind(server_port))
+	# Prepare python environment
+	
+	# Start python server
+	server_thread = Thread.new()
+	server_thread.start(_start_python_ws_server.bind(
+		SERVER_PATH, server_port))

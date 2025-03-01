59867d7a511f1a8fc17aa5be4db6a2f73b04824a
46768 <46768@bodin.ac.th>
2025-03-01 13:10:20 +0700

    Python unpacking refactored, conda's python3 binary are working

--- python-binding/server.py
+++ python-binding/server.py
@@ -1,5 +1,7 @@
 import sys
+import numpy as np
 
 print("Hello world! from server.py!")
 print("This is another line!")
 print(f"This server is running on port {sys.argv[1]}")
+print(np.arange(10))
--- scripts/python-binding/python-binding.gd
+++ scripts/python-binding/python-binding.gd
@@ -7,20 +7,42 @@ extends Node
 # Python environment variables
 const PYTHON_PACKED_PATH: String = "res://python-binding/python-packed.pypck"
 const PYTHON_ENV_PATH: String = "user://python-env"
+var USR_PACKED_PATH: String = PYTHON_ENV_PATH+"/"+PYTHON_PACKED_PATH.get_file()
+const PYTHON_CONDA_PATH: String = PYTHON_ENV_PATH + "/conda"
+const PYTHON3_BIN_PATH: String = PYTHON_CONDA_PATH+"/bin/python3"
+
+# Unpacking cmd table
+const UNPACK_TABLE: Dictionary = {
+	"Windows": ["Expand-Archive", "-LiteralPath", "-DestinationPath"],
+	"Linux": ["tar", "-xvzf", "-C"],
+}
 
 # Server variables
 var server_thread: Thread
 const SERVER_PATH: String = "res://python-binding/server.py"
 var server_output = []
 
-func _start_python_ws_server(server_path: String, port: int, python3_bin: String):
+func _start_python_ws_server(
+	server_path: String, port: int, python3_bin: String
+) -> void:
 	var gpath = ProjectSettings.globalize_path(server_path)
+	var bin_path = ProjectSettings.globalize_path(python3_bin)
 	OS.execute(
-		python3_bin, [gpath, "%d" % port],
+		bin_path, [gpath, "%d" % port],
 		server_output, true
 	)
 	for i in server_output:
 		print(i)
+
+
+func _unpack_python_env(pypck_path: String, dest_path: String) -> void:
+	var unpacker: Array = UNPACK_TABLE[OS.get_name()]
+	var unpack_output: Array = []
+	OS.execute(unpacker[0], [
+		unpacker[1], ProjectSettings.globalize_path(pypck_path),
+		unpacker[2], ProjectSettings.globalize_path(dest_path),
+	], unpack_output)
+	pass
 
 func _ready() -> void:
 	# Prepare python environment
@@ -29,33 +51,17 @@ func _ready() -> void:
 		data_dir.make_dir(PYTHON_ENV_PATH)
 	
 	# Copy packed env if not found
-	var python_usr_packed_path: String = PYTHON_ENV_PATH + "/" + PYTHON_PACKED_PATH.get_file()
-	if not FileAccess.file_exists(python_usr_packed_path):
-		data_dir.copy(PYTHON_PACKED_PATH, python_usr_packed_path)
+	if not FileAccess.file_exists(PYTHON_PACKED_PATH):
+		printerr("Cant find packed env file '%s'" % PYTHON_PACKED_PATH)
+		return
+	if not FileAccess.file_exists(USR_PACKED_PATH):
+		data_dir.copy(PYTHON_PACKED_PATH, USR_PACKED_PATH)
 	
-	if not data_dir.dir_exists(PYTHON_ENV_PATH + "/conda"):
-		var unpacker: String
-		var unpacker_src_f: String
-		var unpacker_dest_f: String
-		var unpack_output = []
-		match OS.get_name():
-			"Windows":
-				unpacker = "Expand-Archive"
-				unpacker_src_f = "-LiteralPath"
-				unpacker_dest_f = "-DestinationPath"
-			"Linux":
-				unpacker = "tar"
-				unpacker_src_f = "-xvzf"
-				unpacker_dest_f = "-C"
-		data_dir.make_dir(PYTHON_ENV_PATH + "/conda")
-		var ret_code: int = OS.execute(unpacker, [
-			unpacker_src_f,
-			ProjectSettings.globalize_path(python_usr_packed_path),
-			unpacker_dest_f,
-			ProjectSettings.globalize_path(PYTHON_ENV_PATH + "/conda"),
-			], unpack_output)
+	if not data_dir.dir_exists(PYTHON_CONDA_PATH):
+		data_dir.make_dir(PYTHON_CONDA_PATH)
+		_unpack_python_env(USR_PACKED_PATH, PYTHON_CONDA_PATH)
 	
 	# Start python server
 	server_thread = Thread.new()
 	server_thread.start(_start_python_ws_server.bind(
-		SERVER_PATH, server_port, "python3"))
+		SERVER_PATH, server_port, PYTHON3_BIN_PATH))

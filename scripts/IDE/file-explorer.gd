6b79f060aba54fd70e6188595b327dcd2bd67242
46768 <46768@bodin.ac.th>
2025-03-21 02:16:49 +0700

    docs(ide): documented filetree script

--- scripts/IDE/file-explorer.gd
+++ scripts/IDE/file-explorer.gd
@@ -5,6 +5,8 @@ const FILE_TEXTURE: Texture2D = preload("res://assets/textures/file-tree/file.sv
 const DIR_TEXTURE: Texture2D = preload("res://assets/textures/file-tree/folder.svg")
 const DIR_OPEN_TEXTURE: Texture2D = preload("res://assets/textures/file-tree/folder-opened.svg")
 
+
+## A class for building a file tree from a VFS into a Tree node
 class VFSTree:
 	var attached_vfs: VFS
 	var attached_tree: Tree
@@ -27,17 +29,25 @@ class VFSTree:
 		
 		var item_dict: Dictionary = {}
 		var bfs_queue: PackedStringArray = PackedStringArray(["/"])
+
+		# Create root tree item
 		item_dict["/"] = attached_tree.create_item()
 		item_dict["/"].set_text(0, "/")
 		item_dict["/"].set_icon(0, DIR_TEXTURE)
 		
 		while not bfs_queue.is_empty():
+			# Pop path from queue
 			var current_path: String = bfs_queue[0]
 			bfs_queue.remove_at(0)
+
 			var current_block: Dictionary = attached_vfs.get_block(current_path)
 			var is_dir: bool = attached_vfs.is_dir(current_path)
+
+			# Add sub-blocks to queue
 			if is_dir:
 				bfs_queue.append_array((current_block.content as Dictionary).keys())
+
+			# Create tree item for current path
 			if current_path != "/":
 				var parent_item: TreeItem = item_dict[VFS.get_parent(current_path)]
 				item_dict[current_path] = attached_tree.create_item(parent_item)
@@ -62,3 +72,4 @@ func _on_ide_initialized(ide_vfs: VFS) -> void:
 
 func _on_ide_vfs_changed(new_vfs: VFS) -> void:
 	vfstree.change_vfs(new_vfs)
+	vfstree.build_tree()

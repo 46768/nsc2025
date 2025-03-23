99ae64fc79c4455df73c186703eb7457a598c320
46768 <46768@bodin.ac.th>
2025-03-24 00:34:51 +0700

    chore: cleaned up npc code

--- scripts/core/autoloads/signal-bus.gd
+++ scripts/core/autoloads/signal-bus.gd
@@ -1,4 +1,8 @@
 extends Node
 
 
+@warning_ignore_start("unused_signal")
+
 signal on_main_initialized
+
+@warning_ignore_restore("unused_signal")
--- scripts/npc/npc.gd
+++ scripts/npc/npc.gd
@@ -2,33 +2,30 @@ extends StaticBody2D
 
 
 @export var npc_data_resource: JSON = null
+@export var npc_texture: Texture2D:
+	set(texture):
+		npc_texture = texture
+		$Sprite2D.set_texture(texture)
+
+@onready var interaction_text: Label = $Label
 
 var npc_data: Dictionary
 var interact_key: StringName
 
 var text_shift: int = 192
-var interaction_text: Label = null
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	npc_data = npc_data_resource.data
 	
 	interact_key = StringName(npc_data.get("interactKey", "<null>"))
 	
-	interaction_text = Label.new()
 	interaction_text.set_text(npc_data.get("interactText", "<null>"))
-	interaction_text.set_horizontal_alignment(HORIZONTAL_ALIGNMENT_CENTER)
-	interaction_text.set_vertical_alignment(VERTICAL_ALIGNMENT_CENTER)
 	_on_screen_resize()
-	interaction_text.hide()
 	
 	Globals.screen_resized.connect(_on_screen_resize)
-	SignalBus.on_main_initialized.connect(_on_main_initialized)
-
-
-func _on_main_initialized() -> void:
-	Globals.player_ui.add_child(interaction_text)
+	SignalBus.on_main_initialized.connect(
+			func()->void: interaction_text.reparent(Globals.player_ui))
 
 
 func _on_screen_resize() -> void:
@@ -36,6 +33,7 @@ func _on_screen_resize() -> void:
 	var screen_size: Vector2 = Globals.screen_size
 	var screen_center: Vector2 = screen_size / 2
 	
+	@warning_ignore("integer_division")
 	interaction_text.set_position(Vector2(0, screen_center.y+(text_shift/2)))
 	interaction_text.set_size(Vector2(screen_size.x, 128)) # Set full screen wide
 

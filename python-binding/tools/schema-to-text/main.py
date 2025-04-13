90741498dbee9ab4c2b0363845b7b3627cd5e08b
46768 <46768@bodin.ac.th>
2025-04-13 09:51:57 -0700

    feat(tool): add schema to text tool

---
+++ python-binding/tools/schema-to-text/main.py
@@ -0,0 +1,36 @@
+import argparse
+import importlib.util
+import sys
+
+cli_parser = argparse.ArgumentParser(prog="schematotext",
+                                     description="Converts schemas to human "
+                                     + "readable text")
+cli_parser.add_argument("schemaType")
+cli_parser.add_argument("schemaFile")
+
+args = cli_parser.parse_args()
+schema_type = args.schemaType
+schema_file = args.schemaFile
+
+with open(schema_file) as f:
+    schema_data = f.read()
+
+try:
+    importlib.util.find_spec("schema_parsers."+schema_type)
+except ImportError:
+    print(f"Error: Schema '{schema_type}' not found")
+    exit(1)
+
+
+def lazy_import(name):
+    spec = importlib.util.find_spec(name)
+    loader = importlib.util.LazyLoader(spec.loader)
+    spec.loader = loader
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[name] = module
+    loader.exec_module(module)
+    return module
+
+
+parser = lazy_import("schema_parsers."+schema_type)
+parser.parse_schema(schema_data)
---
+++ python-binding/tools/schema-to-text/schema_parsers/json.py
@@ -0,0 +1,5 @@
+import json
+
+
+def parse_schema(json_data: str):
+    print(json_data)
